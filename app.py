c75069d0bb033e47b243422c30afe8e438ba3bc2
rimvydasrub <rimvydasrub@gmail.com>
2024-03-01 17:24:37 +0000

    interface for the codellama tuned

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ import pathlib
 import gradio as gr
 from PIL import Image
 from transformers import pipeline
+
+from peft import PeftModel, PeftConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+
 import wandb
 
 SEED = 42
@@ -13,15 +19,23 @@ N_INTERSECTION = 2
 N_PEDESTRIAN = 2
 MAX_INSTRUCTIONS = 5
 TOTAL_INSTRUCTIONS = 0
+USE_WANDB = False
 
-random.seed(SEED)
+if SEED:
+    random.seed(SEED)
 
 SCENARIOS_BYPASSING = random.sample(list(pathlib.Path('app_data/bypassing').iterdir()), N_BYPASSING)
 SCENARIOS_INTERSECTION = random.sample(list(pathlib.Path('app_data/intersection').iterdir()), N_INTERSECTION)
 SCENARIOS_PEDESTRIAN = random.sample(list(pathlib.Path('app_data/pedestrian').iterdir()), N_PEDESTRIAN)
 SCENARIOS = SCENARIOS_BYPASSING + SCENARIOS_INTERSECTION + SCENARIOS_PEDESTRIAN
 
-pipe = pipeline("text-generation", model="openai-community/gpt2")
+tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-7b-hf", load_in_4bit=True)
+config = PeftConfig.from_pretrained("ipab-rad/codellama2-7b-cdsg-tuned", load_in_4bit=True)
+model = AutoModelForCausalLM.from_pretrained("codellama/CodeLlama-7b-hf", load_in_4bit=True)
+model = PeftModel.from_pretrained(model, "ipab-rad/codellama2-7b-cdsg-tuned", load_in_4bit=True)
+
+
+# pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 def respond(message, chat_history):
     global TOTAL_INSTRUCTIONS
@@ -31,15 +45,23 @@ def respond(message, chat_history):
         responce = "You have reached the maximum number of instructions. Please press next scenario to describe another scenario."
     else:
         TOTAL_INSTRUCTIONS += 1
-        responce = pipe(message)[0]['generated_text']
+        
+        inputs = tokenizer(message, return_tensors="pt").to("cuda")
+        outputs = model.generate(inputs=inputs['input_ids'],
+                                 max_new_tokens=1000,
+                                 do_sample=True,
+                                 temperature=0.1)
+
+        output = outputs[0].cpu()
+        responce = tokenizer.decode(output, skip_special_tokens=True)
 
     chat_history.append((message, responce))
 
     # run a simulation
-    # process = subprocess.Popen(['bash', 'scripts/simulate.sh', '19.scenic'], stdout=subprocess.PIPE)
+    process = subprocess.Popen(['bash', 'scripts/simulate.sh', '19.scenic'], stdout=subprocess.PIPE)
 
     # Get the output and error (if any)
-    # output, error = process.communicate()
+    output, error = process.communicate()
 
     return "", chat_history
 
@@ -69,11 +91,12 @@ def next_scenario(msg, chat_history, img, check):
     global TOTAL_INSTRUCTIONS
     
     # logging
-    wandb.log({"chat_history": wandb.Html(generate_html_chat(chat_history), inject=False),
-               "stimuli": wandb.Image(img),
-               "number_of_instructions": TOTAL_INSTRUCTIONS,
-               "satisfied": int(check),
-               })
+    if USE_WANDB:
+        wandb.log({"chat_history": wandb.Html(generate_html_chat(chat_history), inject=False),
+                "stimuli": wandb.Image(img),
+                "number_of_instructions": TOTAL_INSTRUCTIONS,
+                "satisfied": int(check),
+                })
 
     # reset
     TOTAL_INSTRUCTIONS = 0
@@ -85,19 +108,20 @@ def next_scenario(msg, chat_history, img, check):
     return msg, chat_history, img, check
 
 
+if USE_WANDB:
 
-wandb.init(project="CDSG-experiments",
-           entity="ipab-rad",
-           )
+    wandb.init(project="CDSG-experiments",
+            entity="ipab-rad",
+            )
 
 
 with gr.Blocks() as demo:
     gr.Markdown("# Conversartional Driving Scenario Generation 🚗 💬")
-    gr.Markdown("This is a chat platform that allows you to generate driving scenarios by provinding scenario descriptions in English. \
-                You are asked to describe the scenario depicted in an image and the dialogue agent (chatbot) will generate the scenario \
-                for you using CARLA simulator. If something is wrong with the simulations provided you are encouraged to provide \
-                follow up instructions (up to 5 times). When you are happy with the simulations produced press next scenario to describe another scenario.\
-                You will be asked to describe 6 scenarios in total.")
+    # gr.Markdown("This is a chat platform that allows you to generate driving scenarios by provinding scenario descriptions in English. \
+    #             You are asked to describe the scenario depicted in an image and the dialogue agent (chatbot) will generate the scenario \
+    #             for you using CARLA simulator. If something is wrong with the simulations provided you are encouraged to provide \
+    #             follow up instructions (up to 5 times). When you are happy with the simulations produced press next scenario to describe another scenario.\
+    #             You will be asked to describe 6 scenarios in total.")
     with gr.Row():
         img, done = next_stimuli()
         chatbot = gr.Chatbot(avatar_images=("app_data/user.png", "app_data/bot.png"), show_label=False)
@@ -109,4 +133,4 @@ with gr.Blocks() as demo:
         next_btn = gr.Button(value="Next Scenario")
         next_btn.click(next_scenario, inputs=[msg, chatbot, img, check], outputs=[msg, chatbot, img, check])
 
-    demo.launch(share=True)
+    demo.launch(share=False)
