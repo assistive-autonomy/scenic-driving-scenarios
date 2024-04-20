d23836ebf40b4e53f7dad3fb42f5fe2341e4e525
rimvydasrub <rimvydasrub@gmail.com>
2024-04-20 11:45:09 +0100

    further fixes

---
+++ src/app.py
@@ -0,0 +1,169 @@
+import random
+import subprocess
+import pathlib  
+import re
+import os
+import html
+import uuid
+import tempfile
+from dataclasses import dataclass
+from PIL import Image
+# Experiment tracking
+import wandb
+import hydra
+from hydra.core.config_store import ConfigStore
+from omegaconf import OmegaConf
+from config import ExpConfig
+import gradio as gr
+# ML libraries
+from huggingface_hub import InferenceClient
+from datasets import load_dataset
+# prompt engineering
+from utils import (make_code_prompt,
+                   generate_program,
+                   run_simulation,
+                   make_summ_prompt,
+                   generate_description)
+
+
+def get_scenarios(cfg: ExpConfig) -> list[pathlib.Path]:
+    """Get scenarios from the given path."""
+    base_path = pathlib.Path(cfg.conv.scenarios.path)
+    bypassing = random.sample(list(base_path/"bypassing".iterdir()),
+                              cfg.conv.scenarios.bypassing)
+    intersection = random.sample(list(base_path/"intersection".iterdir()),
+                                 cfg.conv.scenarios.intersection)
+    pedestrian = random.sample(list(base_path/"pedestrian".iterdir()),
+                               cfg.conv.scenarios.pedestrian)
+    scenarios = bypassing + intersection + pedestrian
+    return scenarios
+
+
+@dataclass
+class AppStatus:
+    scenarios: list[pathlib.Path]
+    num: int = 0
+    max: int = 5
+    description: str = ""
+    cfg: ExpConfig = ExpConfig()
+
+def respond(message:str,
+            chat_history:list[str],
+            status: AppStatus,
+            d2p: dict[str, str]):
+
+    if status.num >= status.max:
+        response = "You have reached the maximum number of instructions. Please press next scenario to describe another scenario."
+    else:
+        status.num += 1
+
+        # if not the first message, generate the descripton
+        if status.description != "":
+            summ_prompt = make_summ_prompt(status.description, message)
+            status.description = generate_description(status.cfg, summ_prompt)
+        else:
+            status.description = message
+        
+        code_prompt = make_code_prompt(status.cfg, status.description, d2p)
+        pred_program = generate_program(status.cfg, code_prompt)
+
+        result = run_simulation(status.cfg, pred_program)
+
+        response = f"Here is the scenario generated from your instructions:\n{result}"
+        
+    return response, chat_history
+
+def next_stimuli(status: AppStatus) -> tuple[gr.Image, AppStatus]:
+    """Get the next stimuli."""
+    if not status.scenarios:
+        path = pathlib.Path('assets/app_data/done.png')
+    else:
+        path = status.scenarios.pop(random.randint(0, len(status.scenarios) - 1))
+    
+    return gr.Image(value=Image.open(path),
+                    show_label=False, 
+                    show_download_button=False,
+                    show_share_button=False), status.scenarios
+
+def next_scenario(msg, chat_history, img, check, status: AppStatus):
+
+    def html_chat(chat_history):
+        html = ""
+        for i, (user, bot) in enumerate(chat_history):
+            user = html.escape(user)
+            bot = f"<pre><code>{html.escape(bot)}</code></pre>"
+            print(bot, flush=True)
+            if i == 0:
+                html += f"<p><strong>User:</strong> {user}</p>"
+                html += f"<p><strong>Bot:</strong><br />{bot}</p>"
+            else:
+                html += f"<p><strong>User:</strong> {user}</p>"
+                html += f"<p><strong>Bot:</strong><br />{bot}</p>"
+        return html
+    
+    # logging
+    if status.cfg.wandb.use_wandb:
+        wandb.log({"dialogue": wandb.Html(html_chat(chat_history),inject=False),
+                "stimuli": wandb.Image(img),
+                "number_of_instructions": status["num"],
+                "satisfied": int(check),
+                })
+    # reset
+    status.num = 0
+    msg = 0
+    chat_history = []
+    img, status = next_stimuli(status)
+    check = False
+
+    return msg, chat_history, img, check, status
+
+cs = ConfigStore.instance()
+cs.store(name="base_config", node=ExpConfig)
+
+@hydra.main(config_path="../config", config_name="rag", version_base=None)
+def main(cfg: ExpConfig):
+
+    if cfg.wandb.use_wandb:
+        config = OmegaConf.to_container(cfg, resolve=False)
+        feed = "-error-feeding" if cfg.model.exceptions.do_feeding else ""
+        wandb.init(project=cfg.wandb.project,
+                   entity=cfg.wandb.entity,
+                   name=f"CONV-{cfg.model.model_name}{feed}",
+                   config=config)
+        
+    random.seed(cfg.seed)
+
+    dataset = load_dataset(cfg.data.dataset_name,
+                           trust_remote_code=True)["train"]
+    d2p = {d: p for d, p in zip(dataset["description"], dataset["program"])}
+    
+    # marker for state status
+    status = AppStatus(scenarios=get_scenarios(cfg),
+                       num=0,
+                       max=cfg.conv.max_instructions,
+                       description="",
+                       cfg=cfg)
+
+    with gr.Blocks() as demo:
+        gr.Markdown("# Conversartional Driving Scenario Generation 🚗 💬")
+        with gr.Row():
+            img, status = next_stimuli(status)
+            chatbot = gr.Chatbot(avatar_images=(cfg.conv.assets.user,
+                                                cfg.conv.assets.bot),
+                                                show_label=False)
+        
+        msg = gr.Textbox(label="Enter your instructions here")
+        msg.submit(respond,
+                   inputs=[msg, chatbot, status, d2p],
+                   outputs=[msg, chatbot, status, d2p])
+        with gr.Row():
+            check = gr.Checkbox(label="Satsified with the scenarios produced")
+            next_btn = gr.Button(value="Next Scenario")
+            next_btn.click(next_scenario,
+                           inputs=[msg, chatbot, img, check, status],
+                           outputs=[msg, chatbot, img, check, status])
+
+    demo.launch(share=cfg.conv.share)
+
+if __name__ == '__main__':
+    main()
--- src/utils.py
+++ src/utils.py
@@ -13,106 +13,27 @@ from llama_index.core.schema import TextNode
 
 from config import ExpConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from huggingface_hub import InferenceClient
 
-def retrieve_examplars(description: str,
-                       d2p: dict[str,str],
-                       retriever_model_name:str,
-                       top_k:int=3) -> dict[str, str]:
-    """retrieve examplars for the given description"""
 
-    Settings.embed_model = HuggingFaceEmbedding(model_name=retriever_model_name)
-    Settings.llm = None # not using LLM for embedding.
-    
-    # form the nodes
-    nodes = [TextNode(id_= idx, text=d) for idx, d in enumerate(d2p.keys())]
-    index = VectorStoreIndex(nodes)
-    retriever = VectorIndexRetriever(index, top_k)
-    query_engine = RetrieverQueryEngine(retriever)
-    examplars = query_engine.query(description)
-
-    return {d2p[n.text]: n.text for n in examplars}
-
-def random_examplars(d2p: dict[str,str], top_k:int=3) -> dict[str, str]:
-    """retrieve random examplars for the given description"""
-
-    examplars = random.sample(d2p.items(), top_k)
-    return {d: p for p, d in examplars}
-
-def make_examplars(cfg: ExpConfig, description: str, d2p: dict[str, str]):
-    """generate examplars for the given description"""
+def get_examplars(cfg: ExpConfig, description: str, d2p: dict[str, str]):
+    """Get examplars for the given description."""
 
     if cfg.model.random_examples:
-        return random_examplars(d2p, cfg.model.retrieval_top_k)
+        """Random example retrieval."""
+        examplars = random.sample(d2p.items(), cfg.model.retrieval_top_k)
+        return {d: p for p, d in examplars}
     else:
-        return retrieve_examplars(description,
-                                  d2p,
-                                  cfg.model.retriever_model_name,
-                                  cfg.model.retrieval_top_k)
-
-def make_code_prompt(description:str, examplars:dict[str, str]):
-    """generate code prompt from the examplars and description."""
-    
-    examplars = "".join([f"Description**:\n {d}\nProgram:\n{p}\n" for d,p in examplars.items()])
-
-    return f'''[INST] You are a helpful assistant that translates English descriptions to Scenic programs.
-    Scenic is a domain-specific probabilistic programminglanguage for creating distributions over specified scenarios.
-    For driving scenarios, each program has the following blocks:
-    - MAP AND MODEL: importing town assets and enabling simulator;
-    - CONSTANTS: specifying vehicle blueprint and other constants like vehicle speed, brake intensity and safety distance;
-    - AGENT'S BEHAVIOR: describing how individual vehicles behave in the scenario;
-    - SPATIAL RELATIONS: outlining the type of road the scenario needs to be synthesized in (e.g. having or not having intersections)
-    - SCENARIO SPECIFICATION: creating individual vehicles and pedestrians in the specified roads, together with constraints that are required to be true for the full simulation as well as the termination condition.
-    Here are examples of description and programs:
-    {examplars}
-    Now, please translate the following description to a program.Just give the program. No extra information.
-    Description:{description}[/INST]'''
-
-def generate_program_from_prompt(cfg: ExpConfig,
-                                 model: AutoModelForCausalLM,
-                                 tokenizer: AutoTokenizer,
-                                 prompt:str):
-    """Generate Scenic program from the prompt using the model and tokenizer."""
-    inputs = tokenizer(prompt, return_tensors="pt")
-    outputs = model.generate(input_ids=inputs["input_ids"].to("cuda"),
-                            attention_mask=inputs["attention_mask"].to("cuda"),
-                            pad_token_id=tokenizer.eos_token_id,
-                            temperature=cfg.model.temperature,
-                            do_sample=cfg.model.do_sample,
-                            top_k=cfg.model.decoding_top_k,
-                            num_beams=cfg.model.num_beams,
-                            max_new_tokens=cfg.model.max_new_tokens)
-    pred_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    pred_program = postprocess_program(pred_output, prompt)
-
-    return pred_program, pred_output
-
-def postprocess_program(program, prompt):
-    """cleanup after generation"""
-    program = program[len(prompt):]
-    try:
-        program = program[program.index("#"):]
-        end_marker = "terminate when (distance to egoSpawnPt) > TERM_DIST"
-        end_idx = program.find(end_marker) + len(end_marker) - 1
-        program = program[:end_idx]
-    except Exception as e:
-        program = "Invalid program"
-    return program 
-def make_summ_prompt(description:str, feedback:str) -> str:
-    """generate prompt for summarizing description and user's feedback"""
-
-    return f"""[INST] You are a helpful assistant that creates updated driving scenario descriptions.
-    Given a driving scenario description and corresponding feedback regarding the simulation, your task is to generate an updated description that incorporates the feedback.
-    Please ensure that the updated description accurately reflects the intended action based on the feedback received and does not introduce additional information or lose information, like the number of vehicles or pedestrians in the situation. 
-    The description will outline a specific scenario or context, while the feedback will provide information about how the described scenario could have been improved or modified.
-    Be sure to maintain the original meaning and intent of both the initial description and the feedback.
-    Be brief and only return the updated driving scenario description.
-    Description: {description}
-    Feedback: {feedback}[/INST]"""
-
-## Excpetion feeding (in case not compiled program: add it to prompt and re-generate the program)
-def add_exception(prediction, exception, description):
-    instruction = f"[INST] When trying to run this program, I got the following error:\n{str(exception)}\n\nCan you suggest an updated version?\nPlease first describe the exception, analyze what caused it and how it could be avoided in this specific case, writing your reasoning in comment lines starting with # then generate the updated program. As a reminder, the **English description** is {description} [\INST]"
-    return prediction + "\n</s><s>" + instruction
+        """description-similarity based example retrieval."""
+        Settings.embed_model = HuggingFaceEmbedding(model_name=cfg.model.retriever_model_name)
+        Settings.llm = None # not using LLM for embedding.
+        
+        nodes = [TextNode(id_= idx, text=d) for idx, d in enumerate(d2p.keys())]
+        index = VectorStoreIndex(nodes)
+        retriever = VectorIndexRetriever(index, cfg.model.retrieval_top_k)
+        query_engine = RetrieverQueryEngine(retriever)
+        examplars = query_engine.query(description)
+        return {d2p[n.text]: n.text for n in examplars}
 
 def run_simulation(program):
     tmp_scenario_filename = f"tmp_{uuid.uuid4().hex}.scenic"
@@ -149,3 +70,120 @@ def try_compile_and_run(program):
     os.remove(scenario_filename)
     os.remove(scenario_output_filename)
     return rv
+        
+def make_code_prompt(cfg: ExpConfig, description: str, d2p: dict[str, str]):
+    """generate code prompt from the examplars and description."""
+    
+    examplars = get_examplars(cfg, description, d2p)
+    examplars = "".join([f"Description**:\n {d}\nProgram:\n{p}\n" for d,p in examplars.items()])
+
+    return f'''[INST] You are a helpful assistant that translates English descriptions to Scenic programs.
+    Scenic is a domain-specific probabilistic programminglanguage for creating distributions over specified scenarios.
+    For driving scenarios, each program has the following blocks:
+    - MAP AND MODEL: importing town assets and enabling simulator;
+    - CONSTANTS: specifying vehicle blueprint and other constants like vehicle speed, brake intensity and safety distance;
+    - AGENT'S BEHAVIOR: describing how individual vehicles behave in the scenario;
+    - SPATIAL RELATIONS: outlining the type of road the scenario needs to be synthesized in (e.g. having or not having intersections)
+    - SCENARIO SPECIFICATION: creating individual vehicles and pedestrians in the specified roads, together with constraints that are required to be true for the full simulation as well as the termination condition.
+    Here are examples of description and programs:
+    {examplars}
+    Now, please translate the following description to a program.Just give the program. No extra information.
+    Description:{description}[/INST]'''
+
+def generate_program(cfg: ExpConfig,
+                    prompt:str,
+                    model: AutoModelForCausalLM = None,
+                    tokenizer: AutoTokenizer = None) -> str:
+    
+    def postprocess(program, prompt):
+        """cleanup after generation"""
+        program = program[len(prompt):]
+        try:
+            program = program[program.index("#"):]
+            end_marker = "terminate when (distance to egoSpawnPt) > TERM_DIST"
+            end_idx = program.find(end_marker) + len(end_marker) - 1
+            program = program[:end_idx]
+        except Exception as e:
+            program = "Invalid program"
+        return program
+    
+    def error_feeding(prediction, exception, description):
+        ## Excpetion feeding (in case not compiled program: add it to prompt and re-generate the program)
+        instruction = f"""[INST] When trying to run this program, I got the following error:\n{str(exception)}
+        Can you suggest an updated version?
+        Please first describe the exception, analyze what caused it and how it could be avoided in this specific case, writing your reasoning in comment lines starting with # then generate the updated program.
+        As a reminder, the description is {description} [\INST]"""
+        return prediction + "\n</s><s>" + instruction
+
+    """Generate Scenic program"""
+    num_trials = 0
+    if model is None and tokenizer is None:
+        client = InferenceClient(model=cfg.model.client)
+    else:
+        client = None
+    while True:
+        num_trials += 1
+
+        if client is None:
+
+            inputs = tokenizer(prompt, return_tensors="pt")
+            outputs = model.generate(input_ids=inputs["input_ids"].to("cuda"),
+                                    attention_mask=inputs["attention_mask"].to("cuda"),
+                                    pad_token_id=tokenizer.eos_token_id,
+                                    temperature=cfg.model.temperature,
+                                    do_sample=cfg.model.do_sample,
+                                    top_k=cfg.model.decoding_top_k,
+                                    num_beams=cfg.model.num_beams,
+                                    max_new_tokens=cfg.model.max_new_tokens)
+            pred_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        
+        else:
+            pred_output = client.text_generation(prompt=prompt,
+                                                max_length=6000,
+                                                max_new_tokens=1200)
+
+
+        pred_program = postprocess(pred_output, prompt)
+
+        compiled = try_compile_and_run(pred_program)
+
+        if not compiled["compiled"] and \
+            cfg.model.exceptions.do_feeding and \
+            num_trials < cfg.model.exceptions.max_trials:
+            prompt = error_feeding(pred_output, compiled["exception"], prompt)
+        else:
+            return pred_program, pred_output
+
+def make_summ_prompt(description:str, feedback:str) -> str:
+    """generate prompt for summarizing description and user's feedback"""
+
+    return f"""[INST] You are a helpful assistant that creates updated driving scenario descriptions.
+    Given a driving scenario description and corresponding feedback regarding the simulation, your task is to generate an updated description that incorporates the feedback.
+    Please ensure that the updated description accurately reflects the intended action based on the feedback received and does not introduce additional information or lose information, like the number of vehicles or pedestrians in the situation. 
+    The description will outline a specific scenario or context, while the feedback will provide information about how the described scenario could have been improved or modified.
+    Be sure to maintain the original meaning and intent of both the initial description and the feedback.
+    Be brief and only return the updated driving scenario description.
+    Description: {description}
+    Feedback: {feedback}[/INST]"""
+
+def generate_description(cfg: ExpConfig,
+                        model: AutoModelForCausalLM,
+                        tokenizer: AutoTokenizer, 
+                        prompt:str) -> str:
+    """Generate updated description from the prompt"""
+
+    inputs = tokenizer(prompt, return_tensors="pt")
+    outputs = model.generate(input_ids=inputs["input_ids"].to("cuda"),
+                            attention_mask=inputs["attention_mask"].to("cuda"),
+                            pad_token_id=tokenizer.eos_token_id,
+                            temperature=cfg.model.temperature,
+                            do_sample=cfg.model.do_sample,
+                            top_k=cfg.model.decoding_top_k,
+                            num_beams=cfg.model.num_beams,
+                            max_new_tokens=cfg.model.max_new_tokens)
+    pred_description = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return pred_description
+
+
+
