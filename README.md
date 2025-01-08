745f282f61436ba2aad190d6b020499da91af5a0
rimvydasrub <rimvydasrub@gmail.com>
2025-01-08 13:05:20 +0000

    add plotting data

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Conversational Driving Scenario Generation
+# Generating Driving Simulations via Conversation
 
-Generating Driving Scenarios in Conversation for CARLA driving Scenarios
 
-<img src="assets/sample_1.png" width="300" /> <img src="assets/sample_2.png" width="300" /> <img src="assets/sample_3.png" width="300" />
+<img src="assets/overview.png" width="500" /> 
+
+Cyber-physical systems like autonomous vehicles are tested in simulation before deployment, using domain-specific programs for scenario specification. To aid the testing of autonomous vehicles in simulation, we design a natural language interface, using an instruction-following large language model, to assist a non-coding domain expert in synthesising the desired scenarios and vehicle behaviours. We show that using it to convert utterances to the symbolic program is feasible, despite the very small training dataset. Human experiments show that *dialogue* is critical to successful simulation generation, leading to a 4.5 times higher success rate than a generation without engaging in extended conversation.
+
 
 ## Setup
 
@@ -24,4 +26,19 @@ Human Evaluation
 
 ```bash
 python scripts/app.py
+```
+
+
+## References
+
+```
+@misc{rubavicius2024generatingdrivingsimulationsconversation,
+      title={Generating Driving Simulations via Conversation}, 
+      author={Rimvydas Rubavicius and Antonio Valerio Miceli-Barone and Alex Lascarides and Subramanian Ramamoorthy},
+      year={2024},
+      eprint={2410.09829},
+      archivePrefix={arXiv},
+      primaryClass={cs.CL},
+      url={https://arxiv.org/abs/2410.09829}, 
+}
 ```
--- scripts/plot_data.py
+++ scripts/plot_data.py
@@ -2,35 +2,55 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
 
 # Script to be used to plot data for analysis
-data = {
-    'trial_number': range(1, 121),
-    'number_of_turns': np.random.choice([1, 2, 3, 4, 5], size=120),
-    'success': np.random.choice([0, 1], size=120)
-}
+PLOT_WITH_EXECUTION = False
+
+if PLOT_WITH_EXECUTION:
+    data = {
+        'number_of_turns': [1, 2, 3, 4],
+        'intersection': [4.545454545,	9.090909091,	6.818181818,	11.36363636],
+        'bypassing': [5.263157895,	0,	10.52631579,	0],
+        'pedestrian': [8.108108108,	8.108108108,	5.405405405,	8.108108108],
+        'total': [6,	7,	7,	8],
+    }
+else:
+    data = {
+        'number_of_turns': [1, 2, 3, 4],
+        'intersection': [4.545454545,	13.63636364,	4.545454545,	9.090909091],
+        'bypassing': [5.263157895,	5.263157895,	5.263157895,	0],
+        'pedestrian': [8.108108108,	13.51351351,	2.702702703,	5.405405405],
+        'total': [6,	12,	4,	6],
+    }
 
 df = pd.DataFrame(data)
 
-# Sort the dataframe by 'trial_number'
-df = df.sort_values(by='trial_number')
-
-# Calculate cumulative success rate
-grouped = df.groupby('number_of_turns')['success']
-
-df['cumulative_success'] = grouped.cumsum() / (grouped.cumcount() + 1)
+# chang to cumulative sum
+df['intersection'] = df['intersection'].cumsum()
+df['bypassing'] = df['bypassing'].cumsum()
+df['pedestrian'] = df['pedestrian'].cumsum()
+df['total'] = df['total'].cumsum()
 
 # Plotting
 sns.set_theme(style="ticks")
 sns.color_palette("Set2")
 plt.figure(figsize=(8, 4))
 
-sns.lineplot(x='number_of_turns', y='cumulative_success', data=df, marker='o', errorbar="sd", err_style="band")
-plt.title('Cumulative Success Rate by Number of Turns', fontsize=14)
-plt.xlabel('Number of Turns', fontsize=12)
-plt.ylabel('Cumulative Success Rate', fontsize=12)
+plt.plot(df['number_of_turns'], df['intersection'], marker='o', label='Intersection')
+plt.plot(df['number_of_turns'], df['bypassing'], marker='o', label='Bypassing')
+plt.plot(df['number_of_turns'], df['pedestrian'], marker='o', label='Pedestrian')
+plt.plot(df['number_of_turns'], df['total'], marker='o', label='Total')
+
+plt.xlabel('Number of dialoge turns', fontsize=12, labelpad=10)
+plt.ylabel('Succesful Scenario Generation (%)', fontsize=12)
 plt.xticks(df['number_of_turns'].unique())
-plt.yticks([0, 0.25, 0.5, 0.75, 1])
 ax = plt.gca()
 ax.grid(True, linestyle="--", linewidth=0.5, color="gray")
+# Use tight layout to add padding
+plt.tight_layout()
+plt.legend(title='Scenario Type', ncols=2)
 plt.show()
